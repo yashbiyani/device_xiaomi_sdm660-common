eb72960d5cb286df77931bd4bd0f624e7b148fd3
OdSazib <odsazib@gmail.com>
2022-07-10 08:54:53 +0000

    sdm660-common: Fix Sensorservice HAL
    Change-Id: I4494817568a84ff38fbda19aced437fe32a34f11

--- sdm660.mk
+++ sdm660.mk
@@ -421,6 +421,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
     android.hardware.sensors@1.0-impl:64 \
     android.hardware.sensors@1.0-service \
     android.frameworks.sensorservice@1.0.vendor
